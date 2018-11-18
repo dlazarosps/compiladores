e479dd2b09917357c327876a3616e0dd3c3d9ab3
dlazarosps <dlspsilva@inf.ufrgs.br>
2018-11-18 16:37:42 -0200

    [UPDATE] codeGen WHILE/DOWHILE

--- etapa5remake/src/AstDoWhile.cpp
+++ etapa5remake/src/AstDoWhile.cpp
@@ -30,10 +30,69 @@ AstDoWhile::~AstDoWhile()
 
 void AstDoWhile::SemanticAnalysis(SemanticAnalyzer* semanticAnalyzer)
 {
-	// TODO
+	this->expression->SemanticAnalysis(semanticAnalyzer);
+	
+	for (unsigned int i = 0; i < this->commands.size(); i++)
+	{
+		this->commands.at(i)->SemanticAnalysis(semanticAnalyzer);
+	}
 }
 
 void AstDoWhile::GenerateCode(CodeGenerator* codeGenerator)
 {
-	// TODO
+	string labelExpr = codeGenerator->CreateLabel();
+	string labelBegin = codeGenerator->CreateLabel();
+	string labelBeTrue = codeGenerator->CreateLabel();
+	string labelEnd = codeGenerator->CreateLabel();
+
+	if(this->testFirst)
+	{
+		// WHILE
+		// Expr
+		codeGenerator->AddInstruction(new InstructionILOC(labelExpr, "nop", "", "", ""));
+		
+		this->expression->GenerateCode(codeGenerator);
+		string expressionRegister = this->expression->GetResultRegister();
+		
+		codeGenerator->AddInstruction(new InstructionILOC("", "jumpI", labelBegin, "", ""));
+
+		// Test Expr
+		codeGenerator->AddInstruction(new InstructionILOC(labelBegin, "cbr", expressionRegister, labelBeTrue, labelEnd));
+
+		codeGenerator->AddInstruction(new InstructionILOC(labelBeTrue, "nop", "", "", ""));
+		// Loop commands
+		for (unsigned int i = 0; i < this->commands.size(); i++)
+		{
+			this->commands.at(i)->GenerateCode(codeGenerator);
+		}
+		codeGenerator->AddInstruction(new InstructionILOC("", "jumpI", labelExpr, "", "")); // return expr to test
+
+		codeGenerator->AddInstruction(new InstructionILOC(labelEnd, "nop", "", "", "")); // out of loop
+	}
+	else
+	{
+		// DO WHILE
+		
+		codeGenerator->AddInstruction(new InstructionILOC(labelBeTrue, "nop", "", "", ""));
+		// Loop commands
+		for (unsigned int i = 0; i < this->commands.size(); i++)
+		{
+			this->commands.at(i)->GenerateCode(codeGenerator);
+		}
+		codeGenerator->AddInstruction(new InstructionILOC("", "jumpI", labelExpr, "", ""));
+
+		// Expr
+		codeGenerator->AddInstruction(new InstructionILOC(labelExpr, "nop", "", "", ""));
+
+		this->expression->GenerateCode(codeGenerator);
+		string expressionRegister = this->expression->GetResultRegister();
+
+		codeGenerator->AddInstruction(new InstructionILOC("", "jumpI", labelBegin, "", ""));
+
+		// Test Expr
+		codeGenerator->AddInstruction(new InstructionILOC(labelBegin, "cbr", expressionRegister, labelBeTrue, labelEnd));
+
+		codeGenerator->AddInstruction(new InstructionILOC(labelEnd, "nop", "", "", "")); // out of loop
+	}
+
 }
